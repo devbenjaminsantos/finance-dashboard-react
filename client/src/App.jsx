fc0cab2c96e88a18dfefcf506039523161e511bf
Benja <benjaminmontenegro.bm@gmail.com>
2026-04-10 21:08:18 -0300

    fix: correcting route names

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -36,7 +36,7 @@ export default function App() {
             />
 
             <Route
-              path="/transações"
+              path="/transacoes"
               element={
                 <ProtectedRoute>
                   <Transactions />
